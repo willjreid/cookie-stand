5e85ee009e7dcb8742d0e8e54d96194aebd6a898
William J Reid <willjreid@gmail.com>
2017-08-14 18:10:31 -0700

    commiting ability to write to the dom

--- app.js
+++ app.js
@@ -14,13 +14,25 @@ var store1 = {
       var customers = Math.floor(Math.random() * (this.maxHrCustomers - this.minHrCustomers) + this.minHrCustomers);
       var hourlySales = Math.floor(customers * this.avgCookiesSale);
       console.log(hourlySales);
-      this.cookiesSold.push[hourlySales];
-      this.dailyCookieSales = this.dailyCookieSales + hourlySales;
-      //console.log(this.dailyCookieSales);
+
+      this.cookiesSold.push(hourlySales);
+      this.dailyCookieSales = this.dailyCookieSales + hourlySales
+
     }
+    return this.cookiesSold;
   }
 };
 
 store1.cookieSales();
 console.log(store1.dailyCookieSales);
-document.getElementByClass('store1').innerHTML = name;
+console.log(store1.cookiesSold);
+document.getElementById('store1Name').innerHTML = store1.name;
+//document.getElementById('store1hour1').innerHTML = store1.dailyCookieSales;
+document.getElementById('store1hour1').innerHTML = store1.cookiesSold[0];
+document.getElementById('store1hour2').innerHTML = store1.cookiesSold[1];
+/*document.getElementByClass('<h3 class ='store1'></h3>').innerHTML = '<h3 class ='store1'></h3>';
+
+/*
+var store1Name = document.getElementByClass('store1');
+var showTextContent = store1Name.textContent;
+var */
