e1448df04672aac46774842cb0985d42c54fbb62
William J Reid <willjreid@gmail.com>
2017-08-17 15:43:19 -0700

    the table totals are built dynamically

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var hoursToList = ['', '6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
+var hoursToList = ['Cookies Needed By Location', '6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
+var hourlyTotalSales = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]];
+var grandTotal = 0;
 
 function store (name, minCust, maxCust, avgCookies) {
   this.name = name;
@@ -17,9 +19,12 @@ function store (name, minCust, maxCust, avgCookies) {
       var hourlyCookieSales = Math.ceil(this.avgCookies * this.randomCustomersPerHour());
       this.simCookies.push(hourlyCookieSales);
       this.totalCookieSales += hourlyCookieSales;
+      grandTotal += hourlyCookieSales;
+      hourlyTotalSales[i].push(hourlyCookieSales);
     }
   };
   this.eachHourSales();
+
 } ;
 
 var pike = new store('1st and Pike', 23, 65, 6.3);
@@ -38,9 +43,9 @@ for (var i = 0; i < hoursToList.length + 1; i++) {
   th.innerText = hoursToList[i];
   thead.appendChild(th);
 }
-var storeTotal = document.createElement('th');
+//var storeTotal = document.createElement('th');
 th.innerText = 'Location Total Sales';
-thead.appendChild(storeTotal);
+//thead.appendChild(storeTotal);
 header.appendChild(thead);
 
 var storesArray = [pike, seatac, seattleCenter, capitol, alki];
@@ -57,10 +62,33 @@ for (var j = 0; j < storesArray.length; j++) {
     td.innerText = storesArray[j].simCookies[k];
     tr.appendChild(td);
   }
+  var td = document.createElement('td');
+  td.innerText = storesArray[j].totalCookieSales;
+  tr.appendChild(td);
   rows.appendChild(tr);
 };
+
+var footer = document.getElementById('salesTable');
+var tr = document.createElement('tr');
+var td = document.createElement('td');
+td.class = 'footer';
+td.innerText = 'Total Sales: ';
+tr.appendChild(td);
+for (var i = 1; i < hoursToList.length + 1; i++){
+  var td = document.createElement('td');
+  td.innerText = hourlyTotalSales[i].reduce(function(sum,value) {
+    return sum + value ;
+  },0);
+  td.class = 'footer';
+  tr.appendChild(td);
+};
+var superTotal = document.createElement('td');
+td.innerText = grandTotal;
+//tr.appendChild(superTotal);
+footer.appendChild(tr);
+
 //here begins the effort to take form inputs and create a row
-function Post(name, minCust, maxCust, avgCookies) {
+/*function Post(name, minCust, maxCust, avgCookies) {
   this.name = name;
   this.minCust = minCust;
   this.maxCust = maxCust;
@@ -97,7 +125,7 @@ function bakeAndPost(event) {
   post.renderToHTML();
 
 }
-
+/*
 (function() {
   var form = document.getElementById('theForm');
   addEvent(form, 'submit', function(e) {
@@ -105,4 +133,6 @@ function bakeAndPost(event) {
     var elements = this.elements;
     var name = this.elements;
   })
-form.addEventListener('submit', bakeAndPost);
+
+  */
+//form.addEventListener('submit', bakeAndPost);
