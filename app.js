623b55f6b28ef50ebdf82091344bc536e24eb2e8
William J Reid <willjreid@gmail.com>
2017-08-16 21:17:38 -0700

    committing to switch gears

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var storeHours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
+//var storeHours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
 var hoursToList = ['', '6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
 
 function store (name, minCust, maxCust, avgCookies) {
