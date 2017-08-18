dd58e94437ff2eb2a6e5c452dd6df36da1fb0c59
William J Reid <willjreid@gmail.com>
2017-08-17 21:01:24 -0700

    closing out and switching gears

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var hoursToList = ['Cookies Needed By Location', '6:00am', '7:00am', '8:00am', '
 var hourlyTotalSales = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]];
 var grandTotal = 0;
 
-function store (name, minCust, maxCust, avgCookies) {
-  this.name = name;
+function Store (storeName, minCust, maxCust, avgCookies) {
+  this.storeName = storeName;
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.avgCookies = avgCookies;
@@ -24,14 +24,13 @@ function store (name, minCust, maxCust, avgCookies) {
     }
   };
   this.eachHourSales();
-
 } ;
 
-var pike = new store('1st and Pike', 23, 65, 6.3);
-var seatac = new store('SeaTac Airport', 3, 24, 1.2);
-var seattleCenter = new store('Seattle Center', 11, 38, 3.7);
-var capitol = new store('Capitol Hill', 20, 38, 2.3);
-var alki = new store('Alki', 2, 16, 4.6);
+var pike = new Store('1st and Pike', 23, 65, 6.3);
+var seatac = new Store('SeaTac Airport', 3, 24, 1.2);
+var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
+var capitol = new Store('Capitol Hill', 20, 38, 2.3);
+var alki = new Store('Alki', 2, 16, 4.6);
 
 //adding hours to the header row
 var header = document.getElementById('salesTable');
@@ -50,43 +49,78 @@ header.appendChild(thead);
 
 var storesArray = [pike, seatac, seattleCenter, capitol, alki];
 
-var rows = document.getElementById('salesTable');
-for (var j = 0; j < storesArray.length; j++) {
+Store.prototype.rowMaker = function () {
+  var row = document.getElementById('salesTable');
   var tr = document.createElement('tr');
-  th.id = 'store' + (j - 1);
   var th = document.createElement('th');
-  th.innerText = storesArray[j].name;
+  th.innerText = this.storeName;
   tr.appendChild(th);
   for (var k = 0; k < hoursToList.length - 1; k++) {
     var td = document.createElement('td');
-    td.innerText = storesArray[j].simCookies[k];
+    td.innerText = this.simCookies[k];
     tr.appendChild(td);
   }
   var td = document.createElement('td');
-  td.innerText = storesArray[j].totalCookieSales;
+  td.innerText = this.totalCookieSales;
   tr.appendChild(td);
-  rows.appendChild(tr);
+  row.appendChild(tr);
 };
 
-var footer = document.getElementById('salesTable');
-var tr = document.createElement('tr');
-var td = document.createElement('td');
-td.class = 'footer';
-td.innerText = 'Total Sales: ';
-tr.appendChild(td);
-for (var i = 1; i < hoursToList.length + 1; i++){
+for (var i = 0; i < storesArray.length; i++) {
+  storesArray[i].rowMaker();
+}
+//var rowMaker = function () {
+  //for (var j = 0; j < storesArray.length; j++) {
+    //th.id = 'store' + (j - 1);
+//     var td = document.createElement('td');
+//     td.innerText = storesArray[j].totalCookieSales;
+//     tr.appendChild(td);
+//     row.appendChild(tr);
+//   };
+// };
+// rowMaker(event);
+
+var footerMaker = function() {
+  var footer = document.getElementById('salesTable');
+  var tr = document.createElement('tr');
   var td = document.createElement('td');
-  td.innerText = hourlyTotalSales[i].reduce(function(sum,value) {
-    return sum + value ;
-  },0);
   td.class = 'footer';
+  td.innerText = 'Total Sales: ';
   tr.appendChild(td);
+  for (var i = 1; i < hoursToList.length + 1; i++){
+    var td = document.createElement('td');
+    td.innerText = hourlyTotalSales[i].reduce(function(sum,value) {
+      return sum + value ;
+    },0);
+    td.class = 'footer';
+    tr.appendChild(td);
+  };
+  var superTotal = document.createElement('td');
+  td.innerText = grandTotal;
+  //tr.appendChild(superTotal);
+  footer.appendChild(tr);
 };
-var superTotal = document.createElement('td');
-td.innerText = grandTotal;
-//tr.appendChild(superTotal);
-footer.appendChild(tr);
+footerMaker();
 
+function addNewCookieStore(event) {
+  console.log('click');
+  event.preventDefault();
+  var form = event.target;//review this further
+  var formStoreName = form.storeName.value;
+  console.log(formStoreName);
+  var formMinCust = parseInt(form.elements['minCust'].value);
+  console.log(formMinCust);
+  var formMaxCust = parseInt(form.elements['maxCust'].value);
+  var formAvgCookies = parseFloat(form.elements['avgCookies'].value);
+  var newStore = new Store(formStoreName, formMinCust, formMaxCust, formAvgCookies);
+  console.log(newStore);
+  storesArray.push(newStore);
+  newStore.rowMaker();
+  //newStore.footerMaker();
+  theForm.reset();
+}
+var getSubmit = document.getElementById('theForm');
+getSubmit.addEventListener('submit', addNewCookieStore);
 //here begins the effort to take form inputs and create a row
 /*function Post(name, minCust, maxCust, avgCookies) {
   this.name = name;
